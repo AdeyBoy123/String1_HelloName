e7457b0ceca0cf6d18c2c06b0b83f90231429f10
AdeyBoy123 <groveraden@gmail.com>
2023-09-15 15:48:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String1_HelloName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(helloName(name));
+            Console.ReadLine();
+        }
+
+        static string helloName(string name)
+        {
+            return "Hello " + name + "!";
+        }
+    }
+}
